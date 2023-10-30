ae9ac2ca8efbfefa59b77c18bce690ff0d2773ce
wjz <jingzhou_wang@hotmail.com>
2023-10-30 15:09:35 +0800

    BuildConfig.java

---
+++ app/src/main/java/io/github/trojan_gfw/igniter/BuildConfig.java
@@ -0,0 +1,10 @@
+package io.github.trojan_gfw.igniter;
+
+public class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "io.github.trojan_gfw.igniter";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = -1;
+    public static final String VERSION_NAME = "";
+}
